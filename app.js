ff36af5618d2cf3337bee342a0f52578d314d5bc
NattieWalker <natalie.brooks3@gmail.com>
2021-07-07 20:36:04 -0400

    Fixed checkForWin() bug

--- app.js
+++ app.js
@@ -266,7 +266,7 @@ document.addEventListener('DOMContentLoaded',()=> {
 
     // check for win
     function checkForWin() {
-        if (score === 274) {
+        if (score >= 272) {
         ghosts.forEach(ghost => clearInterval(ghost.timerId));
         document.removeEventListener('keyup', movePacman);
         setTimeout(function(){ alert("You won!"); }, 500)
